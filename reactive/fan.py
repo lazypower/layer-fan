f2ae3881ef55b9f439a159cc47b54259054632e9
Charles Butler <chuck@dasroot.net>
2016-02-11 16:16:09 -0500

    BONZAI :beers:

---
+++ reactive/fan.py
@@ -0,0 +1,28 @@
+from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import status_set, config
+from charmhelpers.fetch import apt_install
+
+from shlex import split
+from subprocess import check_call
+
+@when_not('fan.installed')
+def install_fan_modules():
+     ''' Installs the fan networking modules from ARCHIVE '''
+     pkgs = ['ubuntu-fan'] 
+     apt_install(pkgs, fatal=True)
+     set_state('fan.installed')
+
+
+@when('fan.installed')
+def configure_fan_overlay():
+    cfg = config()
+    if cfg.changed('overlay_network') or cfg.changed('underlay_network'):
+        # caveate - this breaks if you juju unset the values
+        cmd = "fanatic configure {0} {1}".format(config('overlay_network'),
+                                                 config('underlay_network'))
+        check_call(split(cmd))
+
+    status_set('active', 'Configured fan networking with: {}'.format(cmd))
+    set_state('fan.configured')
+    # do something with that later
+
